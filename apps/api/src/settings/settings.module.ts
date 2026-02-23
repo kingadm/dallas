37ad0e4fac069a527d6a1cae5321895fbd70b421
kingadm <kauanxplol78@gmail.com>
2026-02-22 21:03:45 -0300

    mensagem do commit

--- apps/api/src/settings/settings.module.ts
+++ apps/api/src/settings/settings.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SettingsController } from "./settings.controller";
 import { SettingsService } from "./settings.service";
+import { CloudinaryModule } from "../cloudinary/cloudinary.module";
 
 @Module({
+  imports: [CloudinaryModule],
   controllers: [SettingsController],
   providers: [SettingsService],
   exports: [SettingsService]
